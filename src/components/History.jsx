d2f6ef99dbfc96a7238a6d0dbd4c369b71255f70
Seviko Attalarik <attalarik.handoko@gmail.com>
2025-02-27 10:39:10 +0700

    fix: text overflow

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -13,10 +13,10 @@ export default function History({id, title, currentID}) {
         {
             currentID == id ? 
             <button className='bg-[var(--color-tertiary)] rounded-xl p-2 w-full flex flex-row' onClick={() => changeURL(id)}>
-                <h1 id={id} className='text-[var(--color-text)] text-lg mr-3'>{title}</h1>
+                <h1 id={id} className='text-[var(--color-text)] text-lg mr-3 truncate'>{title}</h1>
             </button> :
             <button className='bg-[var(--color-secondary)] hover:bg-[var(--color-tertiary)] rounded-xl p-2 w-full flex flex-row' onClick={() => changeURL(id)}>
-                <h1 id={id} className='text-[var(--color-text)] text-lg mr-3'>{title}</h1>
+                <h1 id={id} className='text-[var(--color-text)] text-lg mr-3 truncate'>{title}</h1>
             </button>
         }
         </>
