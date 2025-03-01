c6c63341544c80382b09cefe5658be676e9b9904
Seviko Attalarik <attalarik.handoko@gmail.com>
2025-03-01 15:54:00 +0700

    add: error handling

--- src/controller/fetch_backend.js
+++ src/controller/fetch_backend.js
@@ -3,23 +3,43 @@ import axios from 'axios';
 const baseUrl = import.meta.env.VITE_BASE_URL;
 
 const getNotes = async () => {
-    const response = await axios.get(`${baseUrl}/notes`);
-    return response.data;
+    try {
+        const response = await axios.get(`${baseUrl}/notes`);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching notes:', error);
+        throw error;
+    }
 };
 
 const createNote = async (data) => {
-    const response = await axios.post(`${baseUrl}/note`, data);
-    return response.data;
-}
+    try {
+        const response = await axios.post(`${baseUrl}/note`, data);
+        return response.data;
+    } catch (error) {
+        console.error('Error creating note:', error);
+        throw error;
+    }
+};
 
 const updateNote = async (id, data) => {
-    const response = await axios.patch(`${baseUrl}/note/${id}`, data);
-    return response.data;
-}
+    try {
+        const response = await axios.patch(`${baseUrl}/note/${id}`, data);
+        return response.data;
+    } catch (error) {
+        console.error(`Error updating note with id ${id}:`, error);
+        throw error;
+    }
+};
 
 const deleteNote = async (id) => {
-    const response = await axios.delete(`${baseUrl}/note/${id}`);
-    return response.data;
-}
+    try {
+        const response = await axios.delete(`${baseUrl}/note/${id}`);
+        return response.data;
+    } catch (error) {
+        console.error(`Error deleting note with id ${id}:`, error);
+        throw error;
+    }
+};
 
 export { getNotes, updateNote, deleteNote, createNote };
