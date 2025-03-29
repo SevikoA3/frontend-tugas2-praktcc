fdde32533ae21b1756c7ece2cb45881cd013946e
Seviko Attalarik <attalarik.handoko@gmail.com>
2025-03-29 21:17:25 +0700

    update: config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [react(), tailwindcss()],
   base: '/frontend-tugas2-praktcc/',
   resolve: {
     alias: {
@@ -18,7 +19,7 @@ export default defineConfig({
   server: {
     port: 8080,
     strictPort: true,
-    host: true,
-    origin: "http://0.0.0.0:8080",
+    host: '0.0.0.0',
+    allowedHosts: ['frontend-note-938071808488.us-central1.run.app', '.localhost', '.127.0.0.1'],
   },
 })
